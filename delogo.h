74cb0787dd8abd8bbfa4c9802f0332ab9dce712a
makki_d <d.makiuchi@gmail.com>
2015-09-18 23:28:33 +0900

    2003-10-01

---
+++ delogo.h
@@ -0,0 +1,160 @@
+//
+//	Copylight (C) 2003 MakKi
+//
+//	AvisynthがGPLなため、このソフトもGPLになるらしいです｡
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
+// http://www.gnu.org/copyleft/gpl.html .
+//
+//
+#ifndef ___DELOGO_H
+#define ___DELOGO_H
+
+#include <windows.h>
+#include "avisynth.h"
+#include "logo.h"
+
+#define LOGO_FADE_MAX      256
+#define LOGO_DEFAULT_DEPTH 128
+
+/********************************************************************
+* 	Init関数
+*		Init_ErazeLOGO
+*		Init_AddLOGO
+*********************************************************************/
+extern AVSValue __cdecl Create_EraseLOGO(AVSValue args, void *user_data, IScriptEnvironment *env);
+extern AVSValue __cdecl Create_AddLOGO(AVSValue args, void *user_data, IScriptEnvironment *env);
+
+inline void Init_EraseLOGO(IScriptEnvironment* env)
+{
+	env->AddFunction("EraseLOGO","c[logofile]s[logoname]s[pos_x]i[pos_y]i[depth]i[start]i[fadein]i[fadeout]i[end]i",Create_EraseLOGO, 0);
+}
+inline void Init_AddLOGO(IScriptEnvironment* env)
+{
+	env->AddFunction("AddLOGO","c[logofile]s[logoname]s[pos_x]i[pos_y]i[depth]i[start]i[fadein]i[fadeout]i[end]i",Create_AddLOGO, 0);
+}
+enum {
+	LOGOFILE =1, LOGONAME,
+	POS_X, POS_Y, DEPTH,
+	START, F_IN , F_OUT, END
+};
+
+/*===================================================================
+* 	class deLOGO_Base
+*==================================================================*/
+
+class deLOGO_Base : public GenericVideoFilter {
+
+protected:
+	BYTE logodata[LOGO_MAXSIZE];
+	int  _start, _fadein, _fadeout, _end;
+
+	LOGO_HEADER* lgh;
+
+	deLOGO_Base(const PClip& clip,const char* logofile,const char* logoname,int pos_x,int pos_y,
+			int depth,int start,int fadein,int fadeout,int end,  IScriptEnvironment *env,const char* filtername) : GenericVideoFilter(clip)
+	{
+		const VideoInfo& vi = clip->GetVideoInfo();
+
+		_start   = start;
+		_fadein  = fadein;
+		_fadeout = fadeout;
+		_end     = end;
+
+		child->SetCacheHints(CACHE_NOTHING, 0);
+
+		lgh = (LOGO_HEADER*)logodata;
+		try{
+			ReadLogoData(logofile,logoname);
+			if(pos_x!=0 || pos_y!=0 || depth!=LOGO_DEFAULT_DEPTH)
+				AdjustLogo(pos_x,pos_y,depth);
+		}
+		catch(char* err){
+			env->ThrowError("%s: %s",filtername,err);
+		}
+	}
+	void ReadLogoData(const char* logofile,const char* logoname);
+	void AdjustLogo(int x,int y,int depth);	// AviUtlオリジナルの色空間
+	void Logo_YUY2();
+	void Logo_YV420(){};
+
+	int CalcFade(int n)//,PVideoFrame &src)
+	{
+		if(n<_start || (_end<n && _end>=_start) )	// 範囲外
+			return 0;
+
+		int fade;
+	
+		if(n < _start+_fadein)			// フェードイン
+			fade = ((n -_start)*2 +1)*LOGO_FADE_MAX / (_fadein *2);
+		else if(n > _end-_fadeout && _end>=0)		// フェードアウト
+			fade = ((_end - n)*2 +1)*LOGO_FADE_MAX / (_fadeout *2);
+		else	// 通常
+			fade = LOGO_FADE_MAX;
+
+		return fade;
+	}
+
+	int Abs(int x)
+	{	return ((x<0)? -x : x);	}
+	int Clamp(int n, int l, int h)
+	{	return (n < l) ? l : (n > h) ? h : n;	}
+
+	// ITU-R TB.601に沿ってAviUtlのYCから変換
+	int TB601_Y(int y)
+	{	return ((y +(16<<4)) * 220 +128)/ 256;	}
+	int TB601_C(int c)
+	{	return (c * 225 +128)/ 256;	}
+public:
+
+};
+
+
+/*===================================================================
+* 	class ErazeLOGO
+*==================================================================*/
+
+class EraseLOGO_YUY2 : public deLOGO_Base {
+
+public:
+	EraseLOGO_YUY2(const PClip& clip,const char* logofile,const char* logoname,
+			int pos_x,int pos_y,int depth,int start,int fadein,int fadeout,int end,IScriptEnvironment *env) 
+					: deLOGO_Base(clip,logofile,logoname,pos_x,pos_y,depth,start,fadein,fadeout,end,env,GetName())
+	{	Logo_YUY2();	}
+
+	static const char* GetName(){	return "EraseLOGO";	}
+
+	PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment *env);
+};
+
+/*===================================================================
+* 	class AddLOGO_YUY2
+*==================================================================*/
+
+class AddLOGO_YUY2 : public deLOGO_Base {
+
+public:
+	AddLOGO_YUY2(const PClip& clip,const char* logofile,const char* logoname,
+			int pos_x,int pos_y,int depth,int start,int fadein,int fadeout,int end,IScriptEnvironment *env) 
+					: deLOGO_Base(clip,logofile,logoname,pos_x,pos_y,depth,start,fadein,fadeout,end,env,GetName())
+	{ Logo_YUY2(); }
+
+	static const char* GetName(){	return "AddLOGO";	}
+
+	PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment *env);
+};
+
+
+#endif
---
+++ readme.txt
@@ -0,0 +1,64 @@
+-----------------------------------------------------------------------
+      透過性ロゴ フィルタ for AviSynth 2.5  by MakKi
+-----------------------------------------------------------------------
+
+【機能】
+
+  BS･CS でよく見かける半透明ロゴを付加または除去します。
+
+【書式】
+
+  ・ロゴ付加
+        AddLOGO(logofile="", logoname="",
+                  pos_x=0, pos_y=0, depth=128,
+                  start=0, fadein=0, fadeout=0, end=-1)
+  ・ロゴ除去
+        EraseLOGO(logofile="", logoname="",
+                  pos_x=0, pos_y=0, depth=128,
+                  start=0, fadein=0, fadeout=0, end=-1)
+
+     logofile        : ロゴファイル名（省略不可）  *.ldp、*.lgdのどちらも使用できます。
+     logoname        : ロゴ名        （省略可）    省略した場合はロゴデータファイルの先頭に保存されているロゴを使用します。
+     pos_x, pox_y    : 位置調整                    1/4ピクセル単位に調整します。
+     depth           : 不透明度（深度）調整        128で100%として調整します。
+     start, end      : 開始･終了フレーム           フレーム番号で指定してください。end<startの時はstart以降の全てのフレームで実行されます。
+     fadein, fadeout : フェードフレーム数          ロゴをフェードさせます。
+
+  start, end 以外のパラメータはAviUtl用透過性ロゴフィルタのパラメータと共通です。
+  ロゴデータの作成にはAviUtl用ロゴ解析プラグインを使用してください。
+
+  ※YUY2専用です。呼び出す前にConvertToYUY2等でYUY2式空間に変換してください。
+
+
+【注意】
+
+  このプログラムはフリーソフトウェアです。
+  このプログラムによって損害を負った場合でも、作者は責任を負いません。
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA,
+    or visit http://www.gnu.org/copyleft/gpl.html .
+
+
+【配布元】
+
+  MakKi's SoftWare
+  http://mksoft.hp.infoseek.co.jp/
+
+【更新履歴】
+
+  2003/10/01   ver 0.01 ・公開
+
+
+mailto:makki_d210@yahoo.co.jp
